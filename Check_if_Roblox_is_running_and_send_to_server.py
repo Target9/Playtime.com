9c90cb844c69ea73eb52f902d7752bd78901a7e0
Target9 <143251387+Target9@users.noreply.github.com>
2023-09-23 13:32:30 +0200

    Add files via upload

---
+++ Check_if_Roblox_is_running_and_send_to_server.py
@@ -0,0 +1,44 @@
+import psutil
+import requests
+import time
+
+SERVER_ENDPOINT = "http://playtime.com/server_side/track_roblox.php"
+PERSON_NAME = "Test"
+GAME_NAME = "Roblox"
+
+def get_roblox_info():
+    for process in psutil.process_iter():
+        try:
+            # If this process name doesn't work, adjust accordingly
+            if "Windows10Universal.exe" in process.name():
+                return True, GAME_NAME
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False, ""
+
+
+def send_status_to_server(status, person, game_name=""):
+    try:
+        data = {
+            "status": status,
+            "person": person,
+            "game": game_name
+        }
+        response = requests.post(SERVER_ENDPOINT, data=data)
+        print(response.text)
+    except:
+        print("Failed to send data to server.")
+
+if __name__ == "__main__":
+    while True:
+        is_running, game_name = get_roblox_info()
+        if is_running:
+            send_status_to_server("playing", PERSON_NAME, GAME_NAME)
+        else:
+            send_status_to_server("not_playing", PERSON_NAME)
+        time.sleep(5)
+
+
+#The roblox process name Windows10Universal.exe or RobloxBeta.exe
+#To run without a Window it's pythonw C:\Users\jeghe\OneDrive\Documents\Hacking_Coding\Python\Playing_Tracker\Check_if_Roblox_is_running_and_send_to_server.py 
+#                             pythonw            Location                                                                    script name
