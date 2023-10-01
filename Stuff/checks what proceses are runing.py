86d1777ac2e2c7e2caff23c33a32a85553db4df4
Target9 <143251387+Target9@users.noreply.github.com>
2023-10-01 19:35:48 +0200

    Delete Stuff directory

--- Stuff/checks what proceses are runing.py
+++
@@ -1,7 +0,0 @@
-import psutil
-
-for process in psutil.process_iter():
-    try:
-        print(process.name())
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
